3f6078c0f20b9c4152c17e5f448f57b7bac90762
Svetlana <semenova.svetlana.nikolaevna@gmail.com>
2016-12-09 12:08:29 +0600

    Improved state machine

---
+++ src/main/java/com/company/formatterImpl/state/States.java
@@ -0,0 +1,76 @@
+package com.company.formatterImpl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * States of formatter.
+ */
+
+public class States {
+    /**
+     * Instance of IState.
+     */
+    private IState currentState;
+
+    /**
+     * Gives name of the current state.
+     * @return string containing state
+     */
+    public final String getCurrentState() {
+        return this.currentState.toString();
+    }
+
+    /**
+     * Map of states.
+     */
+    private Map<String, SimpleState> map;
+
+    /**
+     * Method for creating object States.
+     */
+    States() {
+        this.map = new HashMap<String, SimpleState>();
+        this.map.put("*multiLineComment", new SimpleState("afterAsterisk"));
+        this.map.put("*afterSlash", new SimpleState("multiLineComment"));
+        this.map.put("/default", new SimpleState("afterSlash"));
+        this.map.put("/newLineDone", new SimpleState("afterSlash"));
+        this.map.put("/afterSlash", new SimpleState("lineComment"));
+        this.map.put("/afterAsterisk", new SimpleState("default"));
+        this.map.put("\nlineComment", new SimpleState("newLineDone"));
+        this.map.put(";default", new SimpleState("newLineDone"));
+        this.map.put("}default", new SimpleState("newLineDone"));
+        this.map.put("\ndefault", new SimpleState("newLineDone"));
+        this.map.put("{default", new SimpleState("newLineDone"));
+        this.map.put(";newLineDone", new SimpleState("newLineDone"));
+        this.map.put("}newLineDone", new SimpleState("newLineDone"));
+        this.map.put("\nnewLineDone", new SimpleState("newLineDone"));
+        this.map.put("{newLineDone", new SimpleState("newLineDone"));
+        this.map.put("\"default", new SimpleState("string"));
+        this.map.put("\"string", new SimpleState("default"));
+        this.map.put("\\string", new SimpleState("escapeSequence"));
+
+        this.map.put("afterSlash", new SimpleState("default"));
+        this.map.put("afterAsterisk", new SimpleState("default"));
+        this.map.put("newLineDone", new SimpleState("default"));
+        this.map.put("escapeSequence", new SimpleState("string"));
+
+        this.currentState = new SimpleState("default");
+    }
+
+    /**
+     * Function of setting new state.
+     * @param symbol symbol from the source
+     */
+    public final void updateState(final char symbol) {
+        String neededKey = symbol + this.currentState.toString();
+        if (this.map.containsKey(neededKey)) {
+                this.currentState = this.map.get(neededKey);
+            } else {
+                if (this.map.containsKey(this.currentState.toString())) {
+                    this.currentState
+                            = this.map.get(this.currentState.toString());
+                }
+            }
+    }
+}
